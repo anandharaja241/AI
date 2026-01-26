74ff07c3b43c97e9608e31dd0f43cb5d50c50080
anandharaja241 <kbanand241@gmail.com>
2026-01-26 14:07:11 +0530

    CRUD for job opening, AI analyzer mock ready  (#3)
    * Add Pom xml file java servlet version
    
    * Add sqllite DB and Insert record into table
    
    * updateConn create method
    
    * update function work to data changed  and no terminal display
    
    * delete  function success
    
    * GetAll, Get operations are added
    
    * Updating gitignore list
    
    * Removing the old diff file
    
    * Updating gitignore list
    
    * js fuction add next page
    
    * CRUD operations are completed except EDIT & UPDATE
    
    * CRUD operations are completed except EDIT & UPDATE
    
    * Edit, Update job opening pages done, resume-analyzer mock ready, using local libraries
    
    * Edit, Update job opening pages done, resume-analyzer mock ready, using local libraries
    
    ---------
    
    Co-authored-by: Maniyan Konar <vvbala1995@gmail.com>

--- src/main/java/com/AI/html/Database.java
+++ src/main/java/com/AI/html/Database.java
@@ -1,43 +1,174 @@
 package com.AI.html;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
 
 public class Database {
     static final String URL = "jdbc:mysql://localhost:3306/demo";
     static final String USER = "root";
     static final String PASS = "";
-    
-    public static boolean addUser(String email, String password) {
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASS)) {
-            
-            // Class.forName("com.mysql.cj.jdbc.Driver");
-            PreparedStatement ps = conn.prepareStatement("INSERT INTO user(email,password) VALUES (?,?)");
-            ps.setString(1, email);
-            ps.setString(2, password);
-            ps.executeUpdate();
-            return true;
+
+    public static Connection connectDB() {
+        final String URL = "jdbc:sqlite:app.db";
+        final String USER = "user1";
+        final String PASS = "testpass";
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Connection established successfully.");
+            // conn.close();
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int create(Connection conn, String table, String columns, List<String> columnList) {
+        if (conn == null || table == null || table.isEmpty()) {
+            System.out.println("Connection is null or table name is invalid.");
+            return 0;
+        }
+        try (Connection connection = conn) {
+            String placeHolders = "";
+            for (int i = 1; i <= columnList.size(); i++) {
+                // if (i == columnList.size()) {
+                //     placeHolders += "?";
+                //     break;
+                // }
+                placeHolders += "?,";
+            }
+            PreparedStatement ps = conn
+                    .prepareStatement("INSERT INTO " + table + " (" + columns + ", createDate) VALUES (" + placeHolders + "?)");
+            for (int i = 0; i < columnList.size(); i++) {
+                ps.setString(i + 1, columnList.get(i));
+            }
+            ps.setString(columnList.size() + 1, getIndianDate());
+            int rowsAffected = ps.executeUpdate();
+            System.out.println(rowsAffected + " record(s) added.");
+
+            return 1;
         } catch (SQLIntegrityConstraintViolationException e) {
-            return false; // Email already exists
+            System.out.println("Record already exists: " + e.getMessage());
+            return -1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public static int update(Connection conn, String id, String role, String exp) {
+        try (Connection connection = conn) {
+            PreparedStatement ps = conn.prepareStatement("UPDATE jobs SET role = ?, exp=? WHERE id = ?");
+            ps.setString(1, role);
+            ps.setString(2, exp);
+            ps.setString(3, id);
+            int rowsaffect = ps.executeUpdate();
+
+            System.out.println(rowsaffect + " Updated");
+            return 1;
+
+        } catch (SQLException e) {
+            System.out.println("Role already exists: ");
+            return -1; // Role already exists
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public static boolean delete(Connection conn, String ids) {
+        try (Connection connection = conn) {
+            String sql = "DELETE FROM jobs WHERE id IN (" + ids + ")"; 
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            System.out.println("Delete");
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public static boolean validateUser(String email, String password) {
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASS)) {
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM user WHERE email=? AND password=?");
-            ps.setString(1, email);
-            ps.setString(2, password);
-            ResultSet rs = ps.executeQuery();
-            return rs.next();
+    public static ResultSet getAll(Connection conn, String table) {
+        ResultSet rs = null;
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " + table);
+            // PreparedStatement ps = conn.prepareStatement("SELECT * FROM user WHERE
+            // email=? AND password=?");
+            rs = ps.executeQuery();
         } catch (Exception e) {
             e.printStackTrace();
-            return false;
         }
+        return rs;
+    }
+
+    public static ResultSet get(Connection conn, String table, String where) {
+        ResultSet rs = null;
+        try {
+            String query = "SELECT * FROM " + table + " WHERE " + where;
+            PreparedStatement ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = Database.connectDB();
+        String table = "jobs";
+        List<String> columnList = new ArrayList<>();
+        columnList.add("Senior developer2");
+        columnList.add("10 years");
+        /*
+         * Create a new record in the database
+         */
+        // Database.create(conn, table, "role,exp", columnList);5
+
+        // conn = Database.connectDB();
+        // Database.update(conn, 22, "Super Senior developer", "190 years");
+
+        // conn = Database.connectDB();
+        /*
+         * Retrieve all records from the table
+         */
+        ResultSet list = Database.getAll(conn, table);
+
+        /*
+         * Retrieve records from the table based on condition
+         */
+        // String condition = "id='2'";
+        // String condition = "id IN (4,5)";
+        // ResultSet list = Database.get(conn, table, condition);
+        // try {
+        //     while (list.next()) {
+        //         System.out.println("ID: " + list.getInt("id") + ", Role: " + list.getString("role") + ", Exp: "
+        //                 + list.getString("exp"));
+        //     }
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
+        // Database.delete(conn, "3,4,5,6,7,8,9,21,22,25,26,27,28.29");
+    }
+
+    public static String getIndianDate() {
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss a");
+        String formattedTime = istTime.format(formatter);
+        return formattedTime;
     }
 }
--- src/main/resources/UI/app.js
+++ src/main/resources/UI/app.js
@@ -6,9 +6,107 @@ $(function () {
     // setActiveNavButton(javaBackend.currentPage);
 
     // Event handlers
-    $('.nav-btn').on('click', navigateTo);
+    $('body').on('click', '.nav-btn', navigateTo);
+    $('.create-btn').on('click', processCreateJob);
+    $('body').on('click', '.edit-nav-btn', setJobId);
+    $('.update-btn').on('click', processUpdateJob);
+    $('body').on('click', '.delete-btn', processDeleteJob);
+
+
+    if (!javaBackend['createJobs']) {
+        $('body').append("Status: Backend createJobs function not found.");
+    }
 });
 
 function navigateTo(evt) {
     javaBackend.navigateTo && javaBackend.navigateTo(evt.currentTarget.dataset.href);
+}
+
+function processCreateJob(evt) {
+    // var action = evt.currentTarget.dataset.action;
+    var role = $('#role').removeClass('is-invalid').val();
+    var exp = $('#experience').removeClass('is-invalid').val();
+    $('.status').html('');
+    $('body').append("Status: 1 Processing...");
+
+    if (!role) {
+        $('.status').append('Please select role').addClass('text-danger');
+        $('#role').addClass('is-invalid').focus();
+        return;
+    } else if (!exp) {
+        $('.status').append('Please select experience').addClass('text-danger');
+        $('#experience').addClass('is-invalid').focus();
+        return;
+    }
+
+    $('body').append("Status: 2 Processing...");
+
+    if (javaBackend['createJobs']) {
+        $('body').append("Status: 3 Processing...");
+        var status = javaBackend.createJobs(role, exp);
+        $('body').append("Status: " + status);
+
+        if (status) {
+            $('.status').addClass('text-danger').append('The create job has successful');
+        } else {
+            $('.status').addClass('text-danger').append('The create job has failed');
+        }
+    }
+}
+
+function processUpdateJob(evt) {
+    // var action = evt.currentTarget.dataset.action;
+    var id = evt.currentTarget.dataset.id;
+    var role = $('#role').removeClass('is-invalid').val();
+    var exp = $('#experience').removeClass('is-invalid').val();
+    $('.status').html('');
+    $('body').append("Status: 1 Processing...");
+
+    if (!role) {
+        $('.status').append('Please select role').addClass('text-danger');
+        $('#role').addClass('is-invalid').focus();
+        return;
+    } else if (!exp) {
+        $('.status').append('Please select experience').addClass('text-danger');
+        $('#experience').addClass('is-invalid').focus();
+        return;
+    }
+
+    $('body').append("Status: 2 Processing...");
+
+    if (javaBackend['updateJobs']) {
+        $('body').append("Status: 3 Processing...");
+        var status = javaBackend.updateJobs(""+id, role, exp);
+        $('body').append("id: " + id);
+
+        if (status) {
+            $('.status').addClass('text-danger').append('The update job has successful');
+        } else {
+            $('.status').addClass('text-danger').append('The update job has failed');
+        }
+    }
+}
+
+function processDeleteJob(evt) {
+    var $deleteBtn = $(evt.currentTarget);
+    var id = evt.currentTarget.dataset.id;
+    $('.status').html('');
+    // $('body').append("Status: Processing...");
+
+    if (javaBackend['deleteJobs']) {
+        var status = javaBackend.deleteJobs(""+id);
+        $('body').append("Status: " + status);
+        if (status) {
+            // $deleteBtn.closest('tr').remove();
+            location.reload();
+        } else {
+            $('.status').addClass('text-danger').append('The delete job has failed');
+        }
+    }
+}
+
+function setJobId(evt) {
+    var id = evt.currentTarget.dataset.id;
+    localStorage.setItem('editJobId', id);
+    // $('body').append("2. editJobId: " + localStorage.getItem('editJobId') + '@'+ id);
 }
